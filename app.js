4ec063685ca3365c99687a2271575b7463fe7c90
Ann Siapno <ann.n.siapno@gmail.com>
2017-06-09 12:22:22 +1000

    Added Angular

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-let express = require('express')
-let app = express()
-let mongoose = require('mongoose')
-let config = require('./config')
-let setupController = require('./controllers/setupController')
-let apiController = require('./controllers/apiController')
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var config = require('./config');
+var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
-let port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
-app.use('/assets', express.static(__dirname + '/public'))
+app.use('/', express.static(__dirname + '/public'));
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-mongoose.connect(config.getDbConnectionString())
-setupController(app)
-apiController(app)
+mongoose.connect(config.getDbConnectionString());
+setupController(app);
+apiController(app);
 
-app.listen(port)
+app.listen(port);
